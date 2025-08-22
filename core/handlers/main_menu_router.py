8ea293bc177b05345655c1f8207e829a4f4be34d
Djju69 <d9955055556@gmail.com>
2025-08-22 10:50:06 +0700

    refactor(handlers): Unify main menu router to use decorators

--- core/handlers/main_menu_router.py
+++ core/handlers/main_menu_router.py
@@ -1,36 +1,66 @@
-from aiogram import Router, F
-from ..utils.locales_v2 import get_text, get_supported_languages
-from .basic import on_help, on_language_select
+from aiogram import F, Router
+from aiogram.types import Message
+
+from ..utils.locales_v2 import translations
 from .category_handlers_v2 import (
-    show_nearest_v2, handle_profile, show_categories_v2,
-    on_restaurants, on_spa, on_hotels, on_transport, on_tours,
-    on_transport_submenu, on_tours_submenu
+    handle_profile,
+    on_hotels,
+    on_restaurants,
+    on_spa,
+    on_tours,
+    on_tours_submenu,
+    on_transport,
+    on_transport_submenu,
+    show_categories_v2,
+    show_nearest_v2,
 )
+from .basic import on_help, on_language_select
 
-main_menu_router = Router(name='main_menu_router')
+main_menu_router = Router(name="main_menu_router")
 
-def create_text_filter(*keys: str):
-    """Создает фильтр F.text.in_() для всех переводов указанных ключей."""
-    all_translations = []
-    for key in keys:
-        all_translations.extend([get_text(key, lang) for lang in get_supported_languages()])
-    return F.text.in_(all_translations)
+# --- Main Menu ---
+@main_menu_router.message(F.text.in_([t.get('choose_category', '') for t in translations.values()]))
+async def _(message: Message): await show_categories_v2(message)
 
-# --- Главное меню ---
-main_menu_router.message.register(show_categories_v2, create_text_filter('choose_category'))
-main_menu_router.message.register(handle_profile, create_text_filter('profile'))
-main_menu_router.message.register(show_nearest_v2, create_text_filter('show_nearest'))
-main_menu_router.message.register(on_help, create_text_filter('help'))
-main_menu_router.message.register(on_language_select, create_text_filter('choose_language'))
+@main_menu_router.message(F.text.in_([t.get('profile', '') for t in translations.values()]))
+async def _(message: Message): await handle_profile(message)
 
-# --- Меню категорий ---
-main_menu_router.message.register(on_restaurants, create_text_filter('category_restaurants'))
-main_menu_router.message.register(on_spa, create_text_filter('category_spa'))
-main_menu_router.message.register(on_hotels, create_text_filter('category_hotels'))
-main_menu_router.message.register(on_transport, create_text_filter('category_transport'))
-main_menu_router.message.register(on_tours, create_text_filter('category_tours'))
+@main_menu_router.message(F.text.in_([t.get('show_nearest', '') for t in translations.values()]))
+async def _(message: Message): await show_nearest_v2(message)
 
-# --- Подменю и кнопка 'Назад' ---
-main_menu_router.message.register(show_categories_v2, create_text_filter('back_to_categories'))
-main_menu_router.message.register(on_transport_submenu, create_text_filter('transport_bikes', 'transport_cars', 'transport_bicycles'))
-main_menu_router.message.register(on_tours_submenu, create_text_filter('tours_group', 'tours_private'))
+@main_menu_router.message(F.text.in_([t.get('help', '') for t in translations.values()]))
+async def _(message: Message): await on_help(message)
+
+@main_menu_router.message(F.text.in_([t.get('choose_language', '') for t in translations.values()]))
+async def _(message: Message): await on_language_select(message)
+
+# --- Category Menu ---
+@main_menu_router.message(F.text.in_([t.get('category_restaurants', '') for t in translations.values()]))
+async def _(message: Message): await on_restaurants(message)
+
+@main_menu_router.message(F.text.in_([t.get('category_spa', '') for t in translations.values()]))
+async def _(message: Message): await on_spa(message)
+
+@main_menu_router.message(F.text.in_([t.get('category_hotels', '') for t in translations.values()]))
+async def _(message: Message): await on_hotels(message)
+
+@main_menu_router.message(F.text.in_([t.get('category_transport', '') for t in translations.values()]))
+async def _(message: Message): await on_transport(message)
+
+@main_menu_router.message(F.text.in_([t.get('category_tours', '') for t in translations.values()]))
+async def _(message: Message): await on_tours(message)
+
+# --- Submenus & Back Buttons ---
+@main_menu_router.message(F.text.in_([t.get('back_to_categories', '') for t in translations.values()]))
+async def _(message: Message): await show_categories_v2(message)
+
+@main_menu_router.message(F.text.in_(
+    [t.get(k, '') for t in translations.values() for k in ['transport_bikes', 'transport_cars', 'transport_bicycles']]
+))
+async def _(message: Message): await on_transport_submenu(message)
+
+@main_menu_router.message(F.text.in_(
+    [t.get(k, '') for t in translations.values() for k in ['tours_group', 'tours_private']]
+))
+async def _(message: Message): await on_tours_submenu(message)
+
